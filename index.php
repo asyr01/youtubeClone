c62a6796e6b74bed53387b9f68ec138395e5586c
example <alisayar0107@gmail.com>
2021-08-02 21:04:00 +0300

    file structure added

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YouTube</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+  </head>
+  <body>
+    <div id="pageContainer">
+      <div id="mastHeadContainer">
+
+      </div>
+
+      <div id="sideNavContainer">
+
+      </div>
+
+      <div id="mainSectionContainer">
+
+      </div>
+    </div>
+  </body>
+</html>
