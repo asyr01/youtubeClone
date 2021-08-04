5cfaa12edf525dd11f741044f766b9106699e983
example <alisayar0107@gmail.com>
2021-08-04 14:47:38 +0300

    jquery and bootstrap added to the project

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>YouTube</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   </head>
   <body>
     <div id="pageContainer">
       <div id="mastHeadContainer">
-
+YouTube
       </div>
 
       <div id="sideNavContainer">
@@ -19,7 +21,7 @@
 
       <div id="mainSectionContainer">
        <div id="mainContentContainer">
-         
+
        </div>
       </div>
     </div>
