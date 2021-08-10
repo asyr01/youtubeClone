9b530dbff44db976ac7a99bd0c17e3e6b3860bc6
example <alisayar0107@gmail.com>
2021-08-10 06:12:56 +0300

    title input created

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -4,17 +4,25 @@ class VideoDetailsFormProvider {
 
     public function createUploadForm() {
         $fileInput = $this->createFileInput();
+        $titleInput = $this->createTitleInput();
         return "<form action='processing.php' method='POST'>
-                  $fileInput;
+                  $fileInput
+                  $titleInput
                </form>
         ";
     }
 
-  private function createFileInput() {
+    private function createFileInput() {
     return "<div class='form-group'>
       <label for='formControlFile1'>Your File</label>
-      <input type='file' class='form-control-file' id='formControlFile1' required>
+      <input type='file' class='form-control-file' name='fileInput'id='formControlFile1' required>
     </div>";
+  }
+
+  private function createTitleInput() {
+  return "<div class='form-group'>
+  <input class='form-control' type='text' placeholder='Title' name='titleInput' aria-label='default input example'>
+  </div>";
   }
 }
 ?>
