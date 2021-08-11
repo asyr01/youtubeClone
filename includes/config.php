75dac454ee9571a59408270e17d9560fd5cefa10
example <alisayar0107@gmail.com>
2021-08-11 06:42:35 +0300

    try statement added

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,13 @@
 <?php 
 ob_start(); // turns on output buffering
-date_default_timezone_set('Europe/Istanbul')
+date_default_timezone_set('Europe/Istanbul'); // Sets Timezone
+
+// Try & Catch statement for PDO connection
+try {
+// con is connection variable, after comma username and password comes.
+ $con = new PDO("mysql:dbname=youtubeclone;host=localhost", "root", "");
+// Errors to debug
+ $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+
 ?>
