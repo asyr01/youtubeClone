4f311abbd09b80d1ebd470a356cbc55c58c56dee
example <alisayar0107@gmail.com>
2021-08-11 06:43:24 +0300

    if there is an error print it, PDOException used

--- includes/config.php
+++ includes/config.php
@@ -8,6 +8,8 @@ try {
  $con = new PDO("mysql:dbname=youtubeclone;host=localhost", "root", "");
 // Errors to debug
  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch(PDOException $e) {
+   echo "Connection failed: ", $e->getMessage();
 }
 
 ?>
