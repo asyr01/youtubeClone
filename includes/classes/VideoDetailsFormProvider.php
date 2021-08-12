15552d0caa7df83b0ada4a9e41b03cbd7bb74d5f
example <alisayar0107@gmail.com>
2021-08-12 05:49:00 +0300

    query created for retrieving data

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -47,6 +47,7 @@ class VideoDetailsFormProvider {
       </select>
    </div>";
     }
+
 }
 
 ?>
--- upload.php
+++ upload.php
@@ -7,6 +7,10 @@
       <?php 
         $formProvider = new VideoDetailsFormProvider();
         echo $formProvider->createUploadForm();
+
+        // Retrieve data from categories table
+        $query = $con->prepare("SELECT * FROM categories");
+        $query->execute();
       ?>
 </div>
 
