42da52f0145105d784ebdfa0d6f37d1b94b3c1fe
example <alisayar0107@gmail.com>
2021-08-12 05:52:07 +0300

    data retrieved and printed to the upload.php

--- upload.php
+++ upload.php
@@ -11,6 +11,10 @@
         // Retrieve data from categories table
         $query = $con->prepare("SELECT * FROM categories");
         $query->execute();
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+          echo $row["name"] . "<br>";
+        }
       ?>
 </div>
 
