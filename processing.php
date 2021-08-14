e895abe3b028e40ef1fa15d36800a839c8d51e8b
example <alisayar0107@gmail.com>
2021-08-14 20:21:17 +0300

    comment aded for todos

--- processing.php
+++ processing.php
@@ -4,5 +4,11 @@
    if(!isset($_POST["uploadButton"])) {
        echo "No file sent to page.";
        exit();
+   } else {
+       // 1) Create file upload data
+
+       // 2) Process video data (upload)
+
+       // 3) Check if upload was sucessful
    }
 ?>
