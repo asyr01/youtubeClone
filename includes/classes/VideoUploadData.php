bc96a2a9cd5499d1a714e70b184855f6283408e1
example <alisayar0107@gmail.com>
2021-08-15 06:24:27 +0300

    VideoUploadData class created

---
+++ includes/classes/VideoUploadData.php
@@ -0,0 +1,10 @@
+<?php 
+  class VideoUploadData {
+    private $videoDataArray, $title, $description, $privacy, $category, $uploadedBy;
+
+    public function __construct($videoDataArray, $title, $description, $privacy, $category, $uploadedBy) {
+
+      }
+  }
+
+?>
