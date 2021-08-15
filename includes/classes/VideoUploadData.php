b14e046e47ab3670b38e0d2454c15b70334e71d8
example <alisayar0107@gmail.com>
2021-08-15 06:27:40 +0300

    variables referenced with this

--- includes/classes/VideoUploadData.php
+++ includes/classes/VideoUploadData.php
@@ -3,7 +3,12 @@
     private $videoDataArray, $title, $description, $privacy, $category, $uploadedBy;
 
     public function __construct($videoDataArray, $title, $description, $privacy, $category, $uploadedBy) {
-
+          $this->videoDataArray = $videoDataArray;
+          $this->title = $title;
+          $this->description = $description;
+          $this->privacy = $privacy;
+          $this->category = $category;
+          $this->uploadedBy = $uploadedBy;
       }
   }
 
