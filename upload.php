a0ebbf01888059ecf1db8813b53db673ceff5cd6
example <alisayar0107@gmail.com>
2021-08-28 11:58:42 +0300

    Bootstrap modal added to inform when processing the data

--- upload.php
+++ upload.php
@@ -10,4 +10,16 @@
       ?>
 </div>
 
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        Please wait. This might take a while.
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php require_once("includes/footer.php");?>
