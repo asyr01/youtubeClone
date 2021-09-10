7bc6e6e5009b3cc64e9a5dbdaf37092266c27897
example <alisayar0107@gmail.com>
2021-09-10 06:13:34 +0300

    checked if logged in, session variable set

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php require_once("includes/header.php");?>
+
+<?php 
+if(isset($_SESSION["userLoggedIn"])) {
+    echo "User is logged in as " . $_SESSION["userLoggedIn"];
+} else {
+    "not logged in";
+}
+?>
+
 <?php require_once("includes/footer.php");?>
