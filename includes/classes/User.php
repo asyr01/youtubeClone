b93e978937e003bead3d00dbac71bfbabd8b110b
example <alisayar0107@gmail.com>
2021-09-14 06:25:03 +0300

    getUsername function created to test sqlData variable

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -4,13 +4,22 @@ class User {
     private $con, $sqlData;
 
     public function __construct($con, $username){
+        // connection variable
         $this->con = $con;
-
+        
+        // query to get user data from db
         $query = $this->con->prepare("SELECT * FROM users WHERE username = :un");
         $query->bindParam(":un", $username);
         $query->execute();
+ 
+        // store data in the sqlData variable
+        $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function getUsername() {
+        return $this->sqlData["username"];
+    }
+
+
 }
-
-
 ?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php 
 if(isset($_SESSION["userLoggedIn"])) {
-    echo "User is logged in as " . $_SESSION["userLoggedIn"];
+    echo "User is logged in as ";
 } else {
     echo "not logged in";
 }
