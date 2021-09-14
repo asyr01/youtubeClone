67c8d19b77828b9591525332de72d16b909b652c
example <alisayar0107@gmail.com>
2021-09-14 06:18:47 +0300

    uery created to reach user data

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -5,6 +5,10 @@ class User {
 
     public function __construct($con, $username){
         $this->con = $con;
+
+        $query = $this->con->prepare("SELECT * FROM users WHERE username = :un");
+        $query->bindParam(":un", $username);
+        $query->execute();
     }
 }
 
