8ae404ae48f3c7d6007d4c4ee7e38095ec5b4a67
example <alisayar0107@gmail.com>
2021-09-16 05:46:49 +0300

    helper functions created for video class

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -20,5 +20,46 @@ class Video {
             $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
         }
     }
+
+    public function getId() {
+        return $this->sqlData["id"];
+    }
+
+    public function getUploadedBy() {
+        return $this->sqlData["uploadedBy"];
+    }
+
+    public function getTitle() {
+        return $this->sqlData["title"];
+    }
+
+    public function getDescription() {
+        return $this->sqlData["description"];
+    }
+
+    public function getPrivacy() {
+        return $this->sqlData["privacy"];
+    }
+
+    public function getFilePath() {
+        return $this->sqlData["filePath"];
+    }
+
+    public function getCategory() {
+        return $this->sqlData["category"];
+    }
+
+    public function getUploadDate() {
+        return $this->sqlData["uploadDate"];
+    }
+
+    public function getViews() {
+        return $this->sqlData["views"];
+    }
+
+    public function getDuration() {
+        return $this->sqlData["duration"];
+    }
+    
 }
 ?>
