9070359688be1f7687bedebf81161c30e926ebd4
example <alisayar0107@gmail.com>
2021-09-16 05:54:14 +0300

    Video class instansiated and tested

--- watch.php
+++ watch.php
@@ -1,11 +1,16 @@
-<?php require_once("includes/header.php");
+<?php 
+require_once("includes/header.php");
+require_once("includes/classes/Video.php");
 
 // if id dpesn't match an existing one
  if(!isset($_GET["id"])) {
   echo "No URL passed into page";
   exit();
  }
- 
+  
+   // Video class instantiation
+  $video = new Video($con, $_GET["id"], $userLoggedInObj);
+  echo $video->getTitle();
 ?>
 
-<?php require_once("includes/footer.php");?>
+<?php require_once("includes/footer.php"); ?>
