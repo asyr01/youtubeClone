74613eb0a449d56af58fea7f359a832b5f1ea61d
example <alisayar0107@gmail.com>
2021-09-20 06:38:47 +0300

    comments arranged

--- watch.php
+++ watch.php
@@ -5,23 +5,24 @@ require_once("includes/header.php");
 require_once("includes/classes/VideoPlayer.php");
 require_once("includes/classes/VideoInfoSection.php");
 
-// if id dpesn't match an existing one
- if(!isset($_GET["id"])) {
-  echo "No URL passed into page";
-  exit();
- }
+    // If there is no id in the url
+      if(!isset($_GET["id"])) {
+        echo "No URL passed into page";
+        exit();
+      }
   
-   // Video class instantiation
-  $video = new Video($con, $_GET["id"], $userLoggedInObj);
-  $video->incrementViews();
-  
+    // Video class instantiation
+      $video = new Video($con, $_GET["id"], $userLoggedInObj);
+      $video->incrementViews();
 ?>
 
 <div class="watchLeftColumn">
    <?php
+     // Create Video Player
      $videoPlayer = new VideoPlayer($video);
      echo $videoPlayer->create(true);
      
+     // Create Video Info
      $VideoInfoSection = new VideoInfoSection($con, $video, $userLoggedInObj);
      echo $VideoInfoSection->create();
     ?>
