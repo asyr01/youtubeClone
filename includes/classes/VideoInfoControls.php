86d9ae8285f14e133f966681e1431f9613937b65
example <alisayar0107@gmail.com>
2021-09-22 12:22:10 +0300

    constructor function created

--- includes/classes/VideoInfoControls.php
+++ includes/classes/VideoInfoControls.php
@@ -2,6 +2,16 @@
 
 class VideoInfoControls {
     
+    private $video, $userLoggedInObj;
+
+    public function __construct($video, $userLoggedInObj){
+        $this->video = $video;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+    
+    public function create() {
+        
+      }
 }
 
 ?>
