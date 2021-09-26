bace0fbe2d2cace864a08adfb6b82de5e05209c9
example <alisayar0107@gmail.com>
2021-09-26 06:15:44 +0300

    id passed to the php file via js ajax

--- ajax/likeVideo.php
+++ ajax/likeVideo.php
@@ -1,4 +1,3 @@
 <?php
-
-
+echo $_POST["videoId"];
 ?>
--- assets/js/videoPlayerActions.js
+++ assets/js/videoPlayerActions.js
@@ -1,4 +1,6 @@
 // It will make an ajax call
 function likeVideo(button, videoId) {
-  $.post('ajax/likeVideo.php').done(function () {});
+  $.post('ajax/likeVideo.php', { videoId: videoId }).done(function (data) {
+    alert(data);
+  });
 }
