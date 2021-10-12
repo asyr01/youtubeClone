a4d251382d6fb37d1564af4c4d1a716969df6515
example <alisayar0107@gmail.com>
2021-10-12 07:50:21 +0300

    ajax call made via subscribe function in userActions

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -1,1 +1,9 @@
-function subscribe() {}
+function subscribe(userTo, userFrom, button) {
+  if (userTo == userFrom) {
+    alert("You can't subscribe to yourself");
+    return;
+  }
+  $.post('ajax/subscribe.php').done(function () {
+    console.log('done');
+  });
+}
--- includes/footer.php
+++ includes/footer.php
@@ -5,5 +5,6 @@
     
     <!-- Scripts  -->
     <script src="./assets/js/comonActions.js"></script>
+    <script src="./assets/js/userActions.js"></script>
   </body>
 </html>
