2b691b39171342281879a3406a15a8681dae3208
example <alisayar0107@gmail.com>
2021-10-14 08:08:04 +0300

    comments added

--- ajax/subscribe.php
+++ ajax/subscribe.php
@@ -1,6 +1,14 @@
 <?php
+  require_once("../includes/config.php");
+
  if(isset($_POST['userTo']) && isset($_POST['userFrom'])) {
-    echo "succeded";
+    // check if the user is subbed
+
+    // if subbed - delete
+
+    // if not subbed - insert
+
+    // return new number of subs
  } else {
      echo "One or more parameters are not passed into subscribe.php the file";
  }
