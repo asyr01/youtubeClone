babc322ba7aa8c0db94dfcdd7fb7593df13c88b7
example <alisayar0107@gmail.com>
2021-10-15 11:32:39 +0300

    subscribe class toggle functionality added, text changed

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -7,6 +7,10 @@ function subscribe(userTo, userFrom, button) {
     function (count) {
       if (count != null) {
         $(button).toggleClass('subscribe unsubscribe');
+        let buttonText = $(button).hasClass('subscribe')
+          ? 'SUBSCRIBE'
+          : 'SUBSCRIBED';
+        $(button).text(buttonText + ' ' + count);
       } else {
         alert('something went wrong');
       }
