f709b445894ebb6239655e28b35a779216697281
example <alisayar0107@gmail.com>
2021-10-16 06:10:15 +0300

    commentSection.php created

---
+++ includes/classes/CommentSection.php
@@ -0,0 +1,18 @@
+<?php
+
+class CommentSection {
+    private $con, $video, $userLoggedInObj;
+
+    public function __construct($con, $video, $userLoggedInObj){
+        $this->video = $video;
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+    
+    public function create() {
+        
+      }
+
+}
+
+?>
