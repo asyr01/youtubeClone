57a3ccf57fd89711729e5b09edf1f332d577bdb8
example <alisayar0107@gmail.com>
2021-10-20 08:12:02 +0300

    postComment function created

---
+++ assets/js/commentActions.js
@@ -0,0 +1,5 @@
+// replyTo for replying a comment, containerClass for adding comment to the DOM
+function postComment(button, postedBy, videoId, replyTo, containerClass) {
+  const textarea = $(button).siblings('textarea');
+  const commentText = textarea.val();
+}
