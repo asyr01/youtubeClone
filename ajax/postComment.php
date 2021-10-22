d61b5e96a03017bf27980455af40bd88ed2424f2
example <alisayar0107@gmail.com>
2021-10-22 10:38:37 +0300

    bindparam method used placeholders binded to a var

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -4,6 +4,10 @@
   // Check if a comment submitted
  if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
         $query = $con->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body) VALUES(:postedBy, :videoId, :responseTo, :body)");
+        $query->bindParam(":postedBy", $postedBy);
+        $query->bindParam(":videoId", $videoId);
+        $query->bindParam(":responseTo", $responseTo);
+        $query->bindParam(":body", $commentText);
     }
  else {
      echo "One or more parameters are not passed into subscribe.php the file";
