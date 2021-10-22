c13e01eb99fec4569f49642d766f935c585b16ae
example <alisayar0107@gmail.com>
2021-10-22 10:24:10 +0300

    insert query added

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -3,7 +3,7 @@
 
   // Check if a comment submitted
  if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
-        echo "success";
+        $query = $con->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body) VALUES(:postedBy, :videoId, :responseTo, :body)");
     }
  else {
      echo "One or more parameters are not passed into subscribe.php the file";
