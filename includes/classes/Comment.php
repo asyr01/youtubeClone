6da0a6dc9e9050b69068b85dd79115287e15980d
example <alisayar0107@gmail.com>
2021-10-23 09:39:51 +0300

    Comment class created

---
+++ includes/classes/Comment.php
@@ -0,0 +1,11 @@
+<?php
+
+class Comment {
+    
+    private $con, $input, $userLoggedInObj, $videoId;
+    public function __construct($con, $input, $userLoggedInObj, $videoId){
+        
+    }
+}
+
+?>
