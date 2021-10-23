2e78ac7511ea1dc7de1bb9fa44600a994b2264a8
example <alisayar0107@gmail.com>
2021-10-23 09:49:18 +0300

    query executed and input set

--- includes/classes/Comment.php
+++ includes/classes/Comment.php
@@ -1,10 +1,20 @@
 <?php
 
 class Comment {
-    
-    private $con, $input, $userLoggedInObj, $videoId;
+    private $con, $sqlData, $userLoggedInObj, $videoId;
+
     public function __construct($con, $input, $userLoggedInObj, $videoId){
-        
+        if(!is_array($input)) {
+            // if input is not an array and it's an id
+            $query = $con->prepare("SELECT * FROM comments where id=:id");
+            $query->bindParam(":id", $input);
+            $query->execute();
+
+            // overwrite input variable
+            $input = $query->fetch(PDO::FETCH_ASSOC);
+        }
+      // if input is sqlData
+      $this->sqlData = $input;
     }
 }
 
