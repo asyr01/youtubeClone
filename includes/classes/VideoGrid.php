edee5319169e8394ff06604a13c011040c5b9c4b
example <alisayar0107@gmail.com>
2021-11-08 13:23:21 +0300

    constructor and variables created

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -1,6 +1,11 @@
 <?php
 class VideoGrid {
-    
+    private $con, $userŞLoggedInObj;
+    private $largeMode = false;
+    private $gridClass = 'videoGrid'
+    public function __construct($con, $userŞLoggedInObj) {
+      $
+    }
 }
 
 ?>
