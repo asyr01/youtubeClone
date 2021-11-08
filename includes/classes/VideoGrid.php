7a27b1f44d187727dcd2bb3edddd1e4652fff27e
example <alisayar0107@gmail.com>
2021-11-08 13:32:16 +0300

    VideoGrid create function declared but empty. VideoGrid referenced at the header

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -2,9 +2,19 @@
 class VideoGrid {
     private $con, $userŞLoggedInObj;
     private $largeMode = false;
-    private $gridClass = 'videoGrid'
+    private $gridClass = 'videoGrid';
+
     public function __construct($con, $userŞLoggedInObj) {
-      $
+      $this->$con = $con;
+      $this->userŞLoggedInObj = $userŞLoggedInObj;
+    }
+    
+    //videos parameter keeps the videos that are going to be rendered, title keeps title as name explains, showfilter is either true or false to ssort the results.
+    public function create($videos, $title, $showFilter) {
+        return "<div class='$this->gridClass'> 
+                    
+                 </div>
+                ";
     }
 }
 
