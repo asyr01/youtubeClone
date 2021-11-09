93980ee7994641d2d7b76bc4d25941ea0ee1c6a1
example <alisayar0107@gmail.com>
2021-11-09 15:36:23 +0300

    generateItems, generateItemsFromVideos functions are declared

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -11,11 +11,25 @@ class VideoGrid {
     
     //videos parameter keeps the videos that are going to be rendered, title keeps title as name explains, showfilter is either true or false to ssort the results.
     public function create($videos, $title, $showFilter) {
+        if($videos == null) {
+          $gridItems = $this->generateItems();
+        }else {
+          $gridItems = $this->generateItemsFromVideos($video);
+        }
         return "<div class='$this->gridClass'> 
                     
                  </div>
                 ";
     }
+
+    public function generateItems(){
+
+    }
+
+    public function generateItemsFromVideos(){
+      
+    }
+
 }
 
 ?>
