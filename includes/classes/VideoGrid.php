b13c01258b068b16cf34345946ed6ccb42dfbcb4
example <alisayar0107@gmail.com>
2021-11-09 17:06:01 +0300

    VideoGridItem class created

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -1,12 +1,12 @@
 <?php
 class VideoGrid {
-    private $con, $userŞLoggedInObj;
+    private $con, $userLoggedInObj;
     private $largeMode = false;
     private $gridClass = 'videoGrid';
 
-    public function __construct($con, $userŞLoggedInObj) {
+    public function __construct($con, $userLoggedInObj) {
       $this->$con = $con;
-      $this->userŞLoggedInObj = $userŞLoggedInObj;
+      $this->userLoggedInObj = $userLoggedInObj;
     }
     
     //videos parameter keeps the videos that are going to be rendered, title keeps title as name explains, showfilter is either true or false to ssort the results.
@@ -31,15 +31,21 @@ class VideoGrid {
     }
 
     public function generateItems(){
+        $query = $this->con->prepare("SELECT * FROM videos ORDER BY RAND() LIMIT 15");
+        $query->execute();
 
+        $elementsHtml = "";
+        while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $video = new Video($this->con, $row, $this->userLoggedInObj);
+        }
     }
 
     public function generateItemsFromVideos(){
       
     }
 
-    public function createGridHeader() {
-
+    public function createGridHeader($title, $showFilter) {
+      return "";
     }
 }
 
