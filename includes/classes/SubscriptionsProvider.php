0475267881586ed949493fb7355f65a2c85da75c
example <alisayar0107@gmail.com>
2021-11-18 13:11:54 +0300

    loop created to get videos with sql

--- includes/classes/SubscriptionsProvider.php
+++ includes/classes/SubscriptionsProvider.php
@@ -10,11 +10,31 @@ class SubscriptionsProvider {
 
     public function getVideos() {
         $videos = array();
+        // Contains all people with subscribeTo
         $subscriptions = $this->userLoggedInObj->getSubscriptions();
 
-        // if(sizeof($subscriptions) > 0) {
+        if(sizeof($subscriptions) > 0) {
+       
+        $condition = "";
+        $i = 0;
+        
+       //   $query = "SELECT * FROM videos WHERE uplaodedBy = user1 or uplaodedBy = user2 OR uplaodedBy = user3";
+       //   $query->bindParam(1, "user1");
+       //   $query->bindParam(2, "user2");
+       //   $query->bindParam(3, "user3");
+        //Loop adds finded user to query to get all videos user 1, user2, user3
+        while($i < sizeof($subscriptions)) {
+           if($i== 0) {
+               // when it's the first loop
+               $condition = "WHERE uploadedBy=?";
+           } else {
+               $condition = " OR uploadedBy=?";
+           }
+           $i++;
+         }
+         $videoSql = "SELECT * FROM videos $condition";
+       }
 
-        // }
 
         return $videos;
     }
