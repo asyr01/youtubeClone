b8b932ce3d07060653795f8a0f679f4072f24810
example <alisayar0107@gmail.com>
2021-11-21 11:51:15 +0300

    getVideos function created to get searchresults from db

--- includes/classes/SearchResultsProvider.php
+++ includes/classes/SearchResultsProvider.php
@@ -10,7 +10,17 @@ class SearchResultsProvider {
 
     public function getVideos($term, $orderBy){
         $query = $this->con->prepare("SELECT * FROM videos WHERE title LIKE CONCAT('%', :term, '%')
-                        OR uploadedBy LIKE  CONCAT('%', :term, '%') ORDER BY $orderBy");
+                                      OR uploadedBy LIKE  CONCAT('%', :term, '%') ORDER BY $orderBy DESC");
+        $query->bindParam(":term", $term);
+        $query->execute();
+
+        $videos = array();
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $video = new Video($this->con, $row, $this->userLoggedInObj);
+            array_push($videos, $video);
+        }
+        return $videos;
     }
 }
 ?> 
