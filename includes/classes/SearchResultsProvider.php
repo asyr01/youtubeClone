3950e4cda5c0da3f2f72b7e6d6bf6e7057706ce5
example <alisayar0107@gmail.com>
2021-11-21 11:42:55 +0300

    Search query created

--- includes/classes/SearchResultsProvider.php
+++ includes/classes/SearchResultsProvider.php
@@ -7,5 +7,10 @@ class SearchResultsProvider {
         $this->con = $con;
         $this->userLoggedInObj = $userLoggedInObj;
     }
+
+    public function getVideos($term, $orderBy){
+        $query = $this->con->prepare("SELECT * FROM videos WHERE title LIKE CONCAT('%', :term, '%')
+                        OR uploadedBy LIKE  CONCAT('%', :term, '%') ORDER BY $orderBy");
+    }
 }
-?>
+?> 
