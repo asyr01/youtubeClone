43e9d895081fde5ce36d2c1108931a97da89feb4
example <alisayar0107@gmail.com>
2021-11-26 10:18:11 +0300

    NavItems created in the create function -navigationMenuProvider

--- includes/classes/NavigationMenuProvider.php
+++ includes/classes/NavigationMenuProvider.php
@@ -8,7 +8,10 @@ class NavigationMenuProvider {
     }
 
     public function create(){
-
+        $menuHtml = $this->createNavItem("Home", 'assets/images/icons/home.png', 'index.php');
+        $menuHtml .= $this->createNavItem("Trending", 'assets/images/icons/trending.png', 'trending.php');
+        $menuHtml .= $this->createNavItem("Subscriptions", 'assets/images/icons/subscriptions.png', 'subscriptions.php');
+        $menuHtml .= $this->createNavItem("Liked Videos", 'assets/images/icons/thumb-up.png', 'likedVideos.php');
     } 
 
     public function createNavItem($text, $icon, $link) {
