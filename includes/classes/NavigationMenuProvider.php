8688181659a7a4a6927b84ae10b8af255f970cb2
example <alisayar0107@gmail.com>
2021-11-26 10:08:16 +0300

    NavigationMenuProvider class created

---
+++ includes/classes/NavigationMenuProvider.php
@@ -0,0 +1,12 @@
+<?php
+class NavigationMenuProvider {
+    private $con, $userLoggedInObj;
+
+    public function __construct($con, $userLoggedInObj) {
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+
+    }
+
+}
+?>
