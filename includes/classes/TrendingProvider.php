276a4ab75e398db2c03afb55f3ca286753762d9c
example <alisayar0107@gmail.com>
2021-11-29 04:23:03 +0300

    TrendingProvider getVideos method created to get trendng videos

--- includes/classes/TrendingProvider.php
+++ includes/classes/TrendingProvider.php
@@ -12,7 +12,15 @@ class TrendingProvider {
         $videos = array();
 
         $query = $this->con->prepare("SELECT * FROM videos WHERE uploadDate >= now() - INTERVAL 7 DAY
-                                      ORDER BY views DESC LIMIT 15");
+                                  ORDER BY views DESC LIMIT 15");
+        $query->execute();
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $video = new Video($this->con, $row, $this->userLoggedInObj);
+            array_push($videos, $video);
+        }
+        
+        return $videos;
     }
 }
 ?>
