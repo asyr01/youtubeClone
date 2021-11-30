7c7556daa7836b1860b615c6a214e3dfe9df1763
example <alisayar0107@gmail.com>
2021-11-30 04:16:10 +0300

    VideoGrid created to contain trending videos, if logic used

--- includes/trending.php
+++ includes/trending.php
@@ -3,5 +3,17 @@ require_once("includes/header.php");
 require_once("includes/classes/TrendingProvider.php");
 
 $trendingProvider = new TrendingProvider($con, $userLoggedInObj);
+$videos = $trendingProvider->getVideos();
+
+$videoGrid = new VideoGrid($con, $userLoggedInObj);
+
 
 ?>
+
+<div class="largeVideoGridContainer">
+    <?php
+        if(sizeof($videos) > 0) {
+            
+        }
+    ?>
+</div>
