6f90f1f19a4c924a2fb6a9f0f886f1a40b99b5ba
example <alisayar0107@gmail.com>
2021-11-30 04:19:49 +0300

    trending videos echoed

--- includes/trending.php
+++ includes/trending.php
@@ -13,7 +13,9 @@ $videoGrid = new VideoGrid($con, $userLoggedInObj);
 <div class="largeVideoGridContainer">
     <?php
         if(sizeof($videos) > 0) {
-            
+            echo $videoGrid->createLarge($videos, "Trending videos uploaded in the last week", false);
+        } else {
+            echo "No trending videos to show";
         }
     ?>
 </div>
