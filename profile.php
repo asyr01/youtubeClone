c69388f95efb7f9335fe66dfb824b25f7aa4c7bc
example <alisayar0107@gmail.com>
2021-12-03 08:48:39 +0300

    username taken from the url

--- profile.php
+++ profile.php
@@ -1,5 +1,10 @@
 <?php
-require_once("includes/header.php")
+require_once("includes/header.php");
 
+if(isset($_GET["username"])) {
+    $profileUsername = $_GET["username"];
+}
 
 ?>
+
+<div><?php echo $profileUsername?></div>
