63e0baeee5019471ccf7636c2fa8469696406ab9
example <alisayar0107@gmail.com>
2021-12-16 21:10:22 +0300

    createUserDetailsForm function created

--- settings.php
+++ settings.php
@@ -1,10 +1,22 @@
 <?php
-require_once("inludes/header.php");
-require_once("inludes/classes/Account.php");
-require_once("inludes/classes/FormSanitizer.php");
-require_once("inludes/classes/Constants.php");
+require_once("includes/header.php");
+require_once("includes/classes/Account.php");
+require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/SettingsFormProvider.php");
+require_once("includes/classes/Constants.php");
 
 if(!User::isLoggedIn()) {
     header("Location: signIn.php");
 }
+
+$formProvider = new SettingsFormProvider();
+
 ?>
+
+<div class='settingsContainer column'>
+    <div class='formSection'>
+        <?php
+            echo $formProvider->createUserDetailsForm();
+        ?>
+    </div>
+</div>
