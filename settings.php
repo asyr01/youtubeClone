15dd0492dfb071d436589b076973e52d2d3ba337
example <alisayar0107@gmail.com>
2021-12-16 20:18:58 +0300

    user redirected if not logged in

--- settings.php
+++ settings.php
@@ -1,5 +1,10 @@
 <?php
 require_once("inludes/header.php");
+require_once("inludes/classes/Account.php");
+require_once("inludes/classes/FormSanitizer.php");
+require_once("inludes/classes/Constants.php");
 
-
+if(!User::isLoggedIn()) {
+    header("Location: signIn.php");
+}
 ?>
