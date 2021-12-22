f90efd7a862281b0ebcf7df31d562d5039030b2a
example <alisayar0107@gmail.com>
2021-12-22 21:20:37 +0300

    checked if user tries to edit their videos

--- editVideo.php
+++ editVideo.php
@@ -4,12 +4,20 @@ require_once("includes/classes/VideoPlayer.php");
 require_once("includes/classes/VideoDetailsFormProvider.php");
 require_once("includes/classes/VideoUploadData.php");
 
+// Checks if user logged im
 if(!User::isLoggedIn()) {
     header("Location: signIn.php");
 }
 
+// Checks if there is videoId parameter at the url bar.
 if(!isset($_GET["videoId"])) {
     echo "No video selected";
+    exit();
+}
+
+$video = new Video($con,$_GET["videoId"],$userLoggedInObj);
+if($video->getUploadedBy() != $userLoggedInObj->getUsername()) {
+    echo "You are not allowed to perform this action";
     exit();
 }
 
