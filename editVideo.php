7feb2272a41feab4add6b5a81fe4346d40c3fca6
example <alisayar0107@gmail.com>
2021-12-22 21:14:09 +0300

    if logical operators added

--- editVideo.php
+++ editVideo.php
@@ -4,5 +4,13 @@ require_once("includes/classes/VideoPlayer.php");
 require_once("includes/classes/VideoDetailsFormProvider.php");
 require_once("includes/classes/VideoUploadData.php");
 
+if(!User::isLoggedIn()) {
+    header("Location: signIn.php");
+}
+
+if(!isset($_GET["videoId"])) {
+    echo "No video selected";
+    exit();
+}
 
 ?>
