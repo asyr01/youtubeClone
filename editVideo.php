37358f789da84da4122ec5fa5d52626c072d4906
example <alisayar0107@gmail.com>
2021-12-22 21:08:43 +0300

    all needed files imported

--- editVideo.php
+++ editVideo.php
@@ -1,5 +1,8 @@
 <?php
 require_once("includes/header.php");
+require_once("includes/classes/VideoPlayer.php");
+require_once("includes/classes/VideoDetailsFormProvider.php");
+require_once("includes/classes/VideoUploadData.php");
 
 
 ?>
