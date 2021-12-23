261e26481b9ae1f1d84c5d2d021834134b75b662
example <alisayar0107@gmail.com>
2021-12-23 08:03:09 +0300

    selectThumbnail class created

--- editVideo.php
+++ editVideo.php
@@ -3,6 +3,7 @@ require_once("includes/header.php");
 require_once("includes/classes/VideoPlayer.php");
 require_once("includes/classes/VideoDetailsFormProvider.php");
 require_once("includes/classes/VideoUploadData.php");
+require_once("includes/classes/SelectThumbnail.php");
 
 // Checks if user logged im
 if(!User::isLoggedIn()) {
@@ -27,6 +28,9 @@ if($video->getUploadedBy() != $userLoggedInObj->getUsername()) {
         <?php
             $videoPlayer = new VideoPlayer($video);
             echo $videoPlayer->create(false);
+
+            $selectThumbnail = new SelectThumbnail($con, $video);
+            echo $selectThumbnail->create();
         ?>
     </div>
 
---
+++ includes/classes/SelectThumbnail.php
@@ -0,0 +1,18 @@
+<?php
+
+class SelectThumbnail {
+
+    private $con, $video;
+
+    public function __construct($con, $video) {
+        $this->con = $con;
+        $this->video = $video;
+    }
+
+    public function create() {
+
+    }
+
+}
+
+?>
