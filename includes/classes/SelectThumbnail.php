bd23826150fd38cc266cace80b091c1b55c87bc6
example <alisayar0107@gmail.com>
2021-12-23 08:10:32 +0300

    query created to get thumbnails and they are fetched to the an array

--- includes/classes/SelectThumbnail.php
+++ includes/classes/SelectThumbnail.php
@@ -10,6 +10,22 @@ class SelectThumbnail {
     }
 
     public function create() {
+        $thumbnailData = $this->getThumbnailsData();
+    }
+
+    private function getThumbnailsData() {
+        $data = array();
+
+        $query = $this->con->prepare("SELECT * FROM thumbnails WHERE videoId = :videoId");
+        $query->bindParam(":videoId", $videoId);
+        $videoId = $this->video->getId();
+
+        $query->execute();
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            // array_push($data, $row);
+            $data[] = $row;
+        }
 
     }
 
