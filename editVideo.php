2894ec42f457dd178b252a668a99022c4f00f220
example <alisayar0107@gmail.com>
2021-12-28 07:08:09 +0300

    arguments passed and VideoUploadData class instantiated

--- editVideo.php
+++ editVideo.php
@@ -23,7 +23,14 @@ if($video->getUploadedBy() != $userLoggedInObj->getUsername()) {
 }
 
 if(isset($_POST["saveButton"])) {
-    
+    $videoData = new VideoUploadData(
+        null,
+        $_POST["titleInput"],
+        $_POST["descriptionInput"],
+        $_POST["privacyInput"],
+        $_POST["categoryInput"],
+        $userLoggedInObj->getUsername(),
+    );
 }
 ?>
 
